2f956a64cfd1185e566737fbaa91fcd78b75110a
Manas Raj <manasraj1357@gmail.com>
2025-07-27 17:30:47 +0530

    Updated tailwind version from 4 to 3.5

---
+++ codevia-crew/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./src/**/*.{js,ts,jsx,tsx,html}",  // or your actual paths
+],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
