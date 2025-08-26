6996d4f8558475ac7eb31f52667a2eefcc1bdfdd
Manas Raj <manasraj1357@gmail.com>
2025-08-26 16:19:20 +0530

    Remove outdated case study and homepage components; add new about and homepage components with updated content and structure.

--- codevia-crew/src/app/about/page.jsx
+++ codevia-crew/src/app/about/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";   
-import About from "@/pages/aboutpage";
+import About from "@/components/aboutpage";
 
 export default function AboutPage() {
   return (
